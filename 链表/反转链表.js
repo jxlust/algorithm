69dd6522fd7c1925893912ad2f8a428bef7cf8f3
jxl <jxljxufe@163.com>
2020-05-08 11:20:45 +0800

    添加递归反转链表注释

--- 链表/反转链表.js
+++ 链表/反转链表.js
@@ -10,7 +10,17 @@ var reverseList = function (head) {
     return cur;
 };
 
-
+/**
+ * 理解：
+ * 1 > 2 > 3 > 4 > 5
+ * 结束条件head.next为null
+ * cur = 5,head = 4
+ * 所以是head.next.next = head,就是递归的精髓 5.next = 4 , 5 > 4 > null
+ * 4.next = null;
+ * 调用栈继续反向指
+ * 
+ * @param {*} head 
+ */
 var reverseList = function (head) {
      if(head === null || head.next === null){
         return head;

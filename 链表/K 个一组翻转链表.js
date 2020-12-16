91bbf84c97abdd64ab24bcdbc6d7106c4a7cd4a9
jxlust <jxljxufe@163.com>
2020-12-16 13:38:43 +0800

    Update K 个一组翻转链表.js

--- 链表/K 个一组翻转链表.js
+++ 链表/K 个一组翻转链表.js
@@ -44,29 +44,74 @@ var reverseKGroup = function (head, k) {
 }
 
 
+
 var reverseKGroup = function (head, k) {
   // 1 -> 2 -> 3 -> 4 -> 5
   // k = 2 : 2->1->4->3->5;
   // k = 3 : 3->2->1->4->5;
-  const reverse = (node) => {
-    let pre = null;
-    let cur = node;
-    // a->b->c->null
-    // a->null
-    while (cur) {
-      let next = cur.next;
-      cur.next = pre;
-      pre = cur;
-      cur = next;
+  const reverse = (startNode, endNode) => {
+    let temp = endNode.next;
+    let p = startNode;
+    while (temp != endNode) {
+      let nx = p.next;
+      p.next = temp;
+      temp = p;
+      p = nx;
     }
-    return pre;
+    return temp;
+
   }
+
   if (k <= 1) return head;
 
+  let parent = new ListNode(-1);
+  parent.next = head;
+  let p = parent; //哨兵
 
+  while (p.next) {
+    let tail = p; // 因为while 判断了p.next，所以tail一定不会第一次就为空
 
+    for (let i = 0; i < k; i++) {
+      tail = tail.next;
+      if (tail === null) {
+        //到链表结尾了，且不满足k个数
+        return parent.next;
+      }
+    }
+    // tail 到了当前组的最后节点
+    // 记录下一组开头节点
+    // let nextStart = tail.next;
+    //核心 翻转
+    let temp = reverse(head, tail);
+    p.next = temp;
+    tail = head;
+    p = tail;
+    head = tail.next;
+
+  }
+  return parent.next;
 
 };
+
+const reverse = (startNode, endNode) => {
+  let temp = endNode.next;
+  let p = startNode;
+  while (temp != endNode) {
+    let nx = p.next;
+    p.next = temp;
+    temp = p;
+    p = nx;
+  }
+  console.log(1, JSON.stringify(temp));
+  console.log(2, p);
+  console.log(3, startNode);
+  console.log(4, JSON.stringify(endNode));
+  // 1 {"val":3,"next":{"val":2,"next":{"val":1,"next":{"val":4,"next":{"val":5,"next":null}}}}}
+  // 2 { val: 4, next: { val: 5, next: null } }
+  // 3 { val: 1, next: { val: 4, next: { val: 5, next: null } } }
+  // 4 {"val":3,"next":{"val":2,"next":{"val":1,"next":{"val":4,"next":{"val":5,"next":null}}}}}
+  return temp;
+}
 
 let link = {
   val: 1,
@@ -85,4 +130,5 @@ let link = {
   }
 }
 
-console.log(1, reverseKGroup(link, 2));
+reverse(link, link.next.next);
+console.log(link);

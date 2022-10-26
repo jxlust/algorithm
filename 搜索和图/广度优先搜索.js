4c73d32240d88f89b213d6ad78e7caf6ab0b272d
jxlust <jxljxufe@163.com>
2022-10-26 13:03:49 +0800

    Update 广度优先搜索.js

--- 搜索和图/广度优先搜索.js
+++ 搜索和图/广度优先搜索.js
@@ -1,93 +1,98 @@
 const Node = function (val, node) {
-    this.val = val;
-    this.node = node;
+  this.val = val;
+  this.node = node;
 }
 
 let testNode = {
-    val: 'A',
+  val: 'A',
+  node: [{
+    val: 'B',
     node: [{
-        val: 'B',
-        node: [{
-            val: 'E',
-            node: null
-        }]
-    }, {
-        val: 'C',
-        node: [{
-            val: 'F',
-            node: [{
-                val: 'G',
-                node: null
-            }]
-        }]
-    }, {
-        val: 'D',
-        node: [{
-            val: 'G',
-            node: null
-        }]
+      val: 'E',
+      node: null
     }]
+  }, {
+    val: 'C',
+    node: [{
+      val: 'F',
+      node: [{
+        val: 'G',
+        node: null
+      }]
+    }]
+  }, {
+    val: 'D',
+    node: [{
+      val: 'G',
+      node: [{
+        val: 'Z',
+        node: null
+      }]
+    }]
+  }]
 }
 
 const BFS = function (node, target) {
-    let queue = [];
-    let step = 0;
-    let size = 0;
-    queue.push(node);
-    while (queue.length) {
-        //不为空
-        step += 1;
-        size = queue.length;
-        for (let i = 0; i < size; i++) {
-            if (queue[i].val === target) {
-                // console.log(queue);
-                return step;
-            }
-            let nextNodes = queue[i].node;
-            if (nextNodes) {
-                for (let v of nextNodes) {
-                    queue.push(v);
-                }
-            }
-            queue.shift(); //第一个弹出，先进先出
+  let queue = [];
+  let step = 0;
+  let size = 0;
+  queue.push(node);
+  while (queue.length) {
+    //不为空
+    step += 1;
+    size = queue.length;
+    for (let i = 0; i < size; i++) {
+      //get first node 
+      let cur = queue[0];
+      if (cur.val === target) {
+        // console.log(queue);
+        return step;
+      }
+      let nextNodes = cur.node;
+      if (nextNodes) {
+        for (let v of nextNodes) {
+          queue.push(v);
         }
-
+      }
+      queue.shift();
     }
-    return -1;
+
+  }
+  return -1;
 }
-console.log(BFS(testNode, 'G'));
+console.log(BFS(testNode, 'F'));
 
 const BFS_SET = function (node, target) {
-    let queue = [];
-    let used = {};
-    let step = 0;
-    let size = 0;
-    queue.push(node);
-    used[node.val] = true;
-    while (queue.length) {
-        //不为空
-        step += 1;
-        size = queue.length;
-        for (let i = 0; i < size; i++) {
-            if (queue[i].val === target) {
-                console.log(queue);
-                return step;
-            }
-            let nextNodes = queue[i].node;
-            if (nextNodes) {
-                for (let v of nextNodes) {
-                    if (!used[v.val]) {
-                        queue.push(v);
-                        used[v.val] = true;
-                    }
+  let queue = [];
+  let used = {};
+  let step = 0;
+  let size = 0;
+  queue.push(node);
+  used[node.val] = true;
+  while (queue.length) {
+    //不为空
+    step += 1;
+    size = queue.length;
+    for (let i = 0; i < size; i++) {
+      let cur = queue.shift(); //第一个弹出，先进先出
+      if (cur.val === target) {
+        // console.log(queue);
+        return step;
+      }
+      let nextNodes = cur.node;
+      if (nextNodes) {
+        for (let v of nextNodes) {
+          if (!used[v.val]) {
+            queue.push(v);
+            used[v.val] = true;
+          }
 
-                }
-            }
-            queue.shift(); //第一个弹出，先进先出
         }
-
+      }
     }
-    return -1;
+
+  }
+  return -1;
 }
 
 console.log(BFS_SET(testNode, 'G'));
